32fd8342abb63fec3cbf5e413b21b1b601734a5a
JulianDbs <jdbsildesmode@gmail.com>
2022-05-17 23:45:07 -0300

    Deleted 'App.java' file in 'app/src/main/java/com/juliandbs/gradle_junit_nested_test_example/' directory. Deleted 'AppTest.java' file in 'app/src/test/java/com/juliandbs/gradle_junit_nested_test_example/' directory.

--- app/src/main/java/com/juliandbs/gradle_junit_nested_test_example/App.java
+++
@@ -1,5 +0,0 @@
-package com.juliandbs.gradle_junit_nested_test_example;
-
-public class App {
-    public static void main(String[] args) {}
-}
--- app/src/test/java/com/juliandbs/gradle_junit_nested_test_example/AppTest.java
+++
@@ -1,6 +0,0 @@
-package com.juliandbs.gradle_junit_nested_test_example;
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-class AppTest {}
